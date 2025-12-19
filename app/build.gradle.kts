3ed36d2cea232c2a78f650a13b737413dc88e0f4
textile86 <textile2006@list.ru>
2025-12-19 16:31:52 +0300

    Step 1 deploy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("org.sonarqube") version "7.2.2.6593"
+    application
 }
 
 group = "hexlet.code"
@@ -10,10 +11,27 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
+}
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.github.johnrengelman:shadow:8.1.1")
+    }
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet.code/App.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("Hello World!");
+            ctx.contentType("text/plain; charset=utf-8");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+}
